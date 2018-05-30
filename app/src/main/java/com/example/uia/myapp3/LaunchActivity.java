e50eb292afc30543f568c3f5d380467bedb47281
uia <uia@olabank.ru>
2018-05-30 12:48:48 +0300

    add views and actions

--- app/src/main/java/com/example/uia/myapp3/LaunchActivity.java
+++ app/src/main/java/com/example/uia/myapp3/LaunchActivity.java
@@ -1,13 +1,38 @@
 package com.example.uia.myapp3;
 
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LaunchActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        mEditText = findViewById(R.id.etStart);
+
+        mButton = findViewById(R.id.buttonEnter);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!TextUtils.isEmpty(mEditText.getText())) {
+                    showMessage(mEditText.getText().toString());
+                }
+            }
+        });
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
